60fd3fc253df8ef5db07eccaaca04c2844cf45e6
mohamed moslemani <moslemanomohamed@gmail.com>
2025-07-23 16:55:48 +0300

    deleteTable operation from the bq

---
+++ CRUD/deleteTable.py
@@ -0,0 +1,15 @@
+from google.cloud import bigquery
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from Vars import project_id, dataset_id
+
+client = bigquery.Client(project=project_id)
+
+tables_to_delete = [
+    f"{project_id}.{dataset_id}.DV360_AlNajm_parsed"
+]
+
+for table_id in tables_to_delete:
+    client.delete_table(table_id, not_found_ok=True)
+    print(f"Deleted: {table_id}")
